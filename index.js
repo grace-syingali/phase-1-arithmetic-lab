47c6d2de7cee812849cdf5f9f320a90c4c5efc3a
grace-syingali <135760083+grace-syingali@users.noreply.github.com>
2023-06-19 22:11:14 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+function multiply(){
+    return 31 * 2
+ }
+ function mathRandom(){
+    return Math.random() * 100 +1
+ }
+
+function mathMod(num1, num2){
+    return num1 % num2
+}
+function mathMax(){
+    return Math.max(1, 2 ,3 ,4 ,5)
+}
